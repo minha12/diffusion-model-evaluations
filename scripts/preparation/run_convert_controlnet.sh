2a1ee429101b6269685bda0c6a30dfb36e8dde9e
minha12 <minhha.x89@gmail.com>
2025-03-26 18:20:10 +0000

    refactor: move this cript to scripts/preparation/

--- scripts/preparation/run_convert_controlnet.sh
+++ scripts/preparation/run_convert_controlnet.sh
@@ -1,4 +1,4 @@
-python scripts/inference/convert_original_controlnet_to_diffusers.py \
+python scripts/preparation/convert_controlnet.py \
   --checkpoint_path /home/ubuntu/models/trained-diffusion/sd21_controlnet/controlnet/epoch=48-step=38856.ckpt \
   --original_config_file /home/ubuntu/models/trained-diffusion/sd21_controlnet/controlnet/config.yaml \
   --dump_path /home/ubuntu/models/trained-diffusion/sd21_controlnet/controlnet \
